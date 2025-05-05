4c567f75258b60d0590a2587f0678bb941f42323
Mark <feranmijogbodo@gmail.com>
2025-05-05 22:15:59 +0100

    project bootstrap

---
+++ src/api/axios-instance.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { config } from "process";
+import { toast } from "sonner";
+
+const axiosInstance = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL });
+
+// A request interceptor is not added because a couple of things that are meant to be done there are not required , like token createDynamicValidationState, and appending metadata to the request
+
+axiosInstance.interceptors.response.use(
+    response => response,
+    error => {
+        console.log(error)
+        // log the error that is returned 
+        // Handle token expiration, logging, etc.
+        if (error.response?.status === 401) {
+            // Optionally redirect to login or refresh token
+            // ÷
+        }
+        return Promise.reject(error);
+    }
+);
+
+
+export default axiosInstance
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { Toaster } from "sonner";
+import QueryProvider from "@/react-query/query-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AntdRegistry>{children}
+        <AntdRegistry>
+          <QueryProvider>{children}</QueryProvider>
           <Toaster richColors position="bottom-right" />
         </AntdRegistry>
       </body>
